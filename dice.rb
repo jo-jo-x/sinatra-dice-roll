dcdd215ac15b8e20769ae1b11daee0d92519346b
Maurice Ford <mauriceford@gmail.com>
2025-03-08 02:07:52 +0000

    final commit

--- dice.rb
+++ dice.rb
@@ -9,12 +9,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<h1>Dice Roll</h1>
-  
-  <ul><p><a href=https://symmetrical-space-pancake-5gr9vqwg45rgh7prj-4567.app.github.dev/dice/2/6>Roll two 6-sided dice</a></p>
-  <p><a href=https://symmetrical-space-pancake-5gr9vqwg45rgh7prj-4567.app.github.dev/dice/2/10>Roll two 10-sided dice</a></p>
-  <p><a href=https://symmetrical-space-pancake-5gr9vqwg45rgh7prj-4567.app.github.dev/dice/1/20>Roll one 20-sided die</a></p>
-  <p><a href=https://symmetrical-space-pancake-5gr9vqwg45rgh7prj-4567.app.github.dev/dice/5/4>Roll five 4-sided dice</a></p></ul>"
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -26,49 +21,61 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..6)
 
+    @rolls.push(die)
+  end
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..10)
 
+    @rolls.push(die)
+  end
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @rolls = []
 
-  outcome = "You rolled a #{die}."
+  1.times do
+    die = rand(1..20)
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  @rolls = []
 
-  outcome = "You rolled #{first_die}, #{second_die}, #{third_die}, #{fourth_die} and a #{fifth_die} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
 
+    @rolls.push(die)
+  end
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
